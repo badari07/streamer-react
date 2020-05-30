4b08bac6483b5539aeccf229a674eb3ab9fe75fd
badari07 <badari0793@gmail.com>
2020-05-31 00:22:05 +0530

    feat: adding redux from

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+  const renderInput = ({ input, label }) => {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  };
+  return (
+    <form className="ui form">
+      <Field name="title" component={renderInput} label="Enter Title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description"
+      />
+    </form>
+  );
 };
 
-export default StreamCreate;
+export default reduxForm({ form: "StreamCreate" })(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-import auth from "./auth";
+import authReducer from "./auth";
+import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
-  auth,
+  auth: authReducer,
+  form: formReducer,
 });
