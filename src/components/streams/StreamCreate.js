c759e05f2ec299db33fd511ee1de686988cba473
badari07 <badari0793@gmail.com>
2020-06-03 23:57:30 +0530

    feat: adding form validate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  const renderInput = ({ input, label }) => {
+const StreamCreate = ({ handleSubmit }) => {
+  const renderInput = ({ input, label, meta }) => {
+    console.log(meta);
     return (
       <div className="field">
         <label>{label}</label>
@@ -10,16 +11,32 @@ const StreamCreate = () => {
       </div>
     );
   };
+  const onSubmit = (formValues) => {
+    console.log(formValues);
+  };
   return (
-    <form className="ui form">
+    <form className="ui form" onSubmit={handleSubmit(onSubmit)}>
       <Field name="title" component={renderInput} label="Enter Title" />
       <Field
         name="description"
         component={renderInput}
         label="Enter Description"
       />
+      <button className="ui button primary">Submit</button>
     </form>
   );
 };
 
-export default reduxForm({ form: "StreamCreate" })(StreamCreate);
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter description";
+  }
+  return errors;
+};
+
+export default reduxForm({ form: "StreamCreate", validate })(StreamCreate);
